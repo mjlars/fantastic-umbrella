479aad9200bcee47d618a5cb274569204634a8a9
mjlars <maflarson@gmail.com>
2022-03-22 17:29:11 -0600

    asdf

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: '127.0.0.1',
       dialect: 'mysql',
+      port: 3306,
       dialectOptions: {
         decimalNumbers: true,
       },
